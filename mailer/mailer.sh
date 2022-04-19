fdf9128441bde26df013eaf1d1c6f15366ee3c50
krishna <krishna@git.rightend.com>
2022-04-19 11:09:34 -0400

    mailer script

--- mailer/mailer.sh
+++ mailer/mailer.sh
@@ -2,15 +2,15 @@
 
 if [[ $# -lt 1 ]]
 then
-  echo "Usage: $0 <smtp file>"
+  echo "Usage: $0 <smtp file> <recipient email>"
   exit 1
 fi
 
 SMTP_FILE=$1
+RECIPIENT_EMAIL=$2
 
 sed 's/\r$//' ${SMTP_FILE} > smtp_creds.txt
-cat smtp_creds.txt
 export $(cat smtp_creds.txt | xargs)
 
-echo "python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $SMTP_USER --template accept_pr"
-python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $SMTP_USER --template accept_pr
+echo "python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template accept_pr"
+python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template accept_pr

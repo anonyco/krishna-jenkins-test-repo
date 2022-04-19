06b9f23106b6eb54b9bc2b4e44cf36ec757e4a8a
krishna <krishna@git.rightend.com>
2022-04-19 12:33:25 -0400

    mailer

--- mailer/mailer.sh
+++ mailer/mailer.sh
@@ -2,15 +2,16 @@
 
 if [[ $# -lt 2 ]]
 then
-  echo "Usage: $0 <smtp file> <recipient email>"
+  echo "Usage: $0 <smtp file> <recipient email> <template>"
   exit 1
 fi
 
 SMTP_FILE=$1
 RECIPIENT_EMAIL=$2
+TEMPLATE=$3
 
 sed 's/\r$//' ${SMTP_FILE} > smtp_creds.txt
 export $(cat smtp_creds.txt | xargs)
 
-echo "python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template accept_pr"
-python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template accept_pr
+echo "python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE"
+python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE

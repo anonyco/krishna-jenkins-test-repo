611a2c9bfab6e9653f08820590cbfafd3120f7ae
krishna <krishna@git.rightend.com>
2022-04-22 09:48:48 -0400

    notification mailer

--- mailer/mailer.py
+++ mailer/mailer.py
@@ -4,32 +4,46 @@ import argparse
 from jinja2 import  Environment, FileSystemLoader
 import os
 
+# initialize argument parsers
 parser = argparse.ArgumentParser()
-parser.add_argument("--smtp_server", required=True)
-parser.add_argument("--smtp_port", required=True)
-parser.add_argument("--smtp_user", required=True)
-parser.add_argument("--smtp_password", required=True)
-parser.add_argument("--to", required=True)
-parser.add_argument("--template", required=True)
+subparser = parser.add_subparsers()
 
-args = parser.parse_args()
+# to send notifications, used by jenkins
+def sendNoticiationMails(args):
+    mailserver = smtplib.SMTP(args.smtp_server,args.smtp_port)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.login(args.smtp_user, args.smtp_password)
 
-print(args)
+    jenv = Environment(loader = FileSystemLoader(f"templates/{args.template}"))
+    env = os.environ
 
-mailserver = smtplib.SMTP(args.smtp_server,args.smtp_port)
-mailserver.ehlo()
-mailserver.starttls()
-mailserver.login(args.smtp_user, args.smtp_password)
-
-jenv = Environment(loader = FileSystemLoader(f"templates/{args.template}"))
-env = os.environ
-
-msg = EmailMessage()
-msg['Subject'] = jenv.get_template("subject.jinja").render(**env)
-msg['From'] = args.smtp_user
-msg['To'] = args.to
-msg.set_content(jenv.get_template("body.jinja").render(**env))
+    msg = EmailMessage()
+    msg['Subject'] = jenv.get_template("subject.jinja").render(**env)
+    msg['From'] = args.smtp_user
+    msg['To'] = args.to
+    msg.set_content(jenv.get_template("body.jinja").render(**env))
 
 
-mailserver.send_message(msg)
-mailserver.quit()
+    mailserver.send_message(msg)
+    mailserver.quit()
+
+# add sendNotificationMails to subparser
+# example call: python mailer.py notificationMailer <Arguments as shown below>
+notificationMailer = subparser.add_parser('notificationMailer')
+
+notificationMailer.add_argument("--smtp_server", required=True)
+notificationMailer.add_argument("--smtp_port", required=True)
+notificationMailer.add_argument("--smtp_user", required=True)
+notificationMailer.add_argument("--smtp_password", required=True)
+notificationMailer.add_argument("--to", required=True)
+notificationMailer.add_argument("--template", required=True)
+
+notificationMailer.set_defaults(func=sendNoticiationMails)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+    args.func(args)
+
--- mailer/mailer.sh
+++ mailer/mailer.sh
@@ -13,5 +13,5 @@ TEMPLATE=$3
 sed 's/\r$//' ${SMTP_FILE} > smtp_creds.txt
 export $(cat smtp_creds.txt | xargs)
 
-echo "python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE"
-python mailer.py --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE
+echo "python mailer.py notificationMailer --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE"
+python mailer.py notificationMailer --smtp_server $SMTP_SERVER --smtp_port $SMTP_PORT --smtp_user $SMTP_USER --smtp_password $SMTP_PASSWORD --to $RECIPIENT_EMAIL --template $TEMPLATE

80fdbcd35af779f4bb99ec1e3999d3eca2ee23cd
krishna <krishna@git.rightend.com>
2022-04-27 11:19:28 -0400

    mailer script

--- mailer/mailer.py
+++ mailer/mailer.py
@@ -1,10 +1,14 @@
 import smtplib
 import imaplib
 from email.message import EmailMessage
+import email
 import argparse
 from jinja2 import  Environment, FileSystemLoader
 import os
 import re
+import sys
+from os import path
+import shutil
 
 SUBJECT_REGEXES = ["^[PATCH [0-9]+/[0-9]+]", "^\[PATCH\]"]
 
@@ -15,23 +19,67 @@ def regexListMatch(regexList, string):
             return x
     return False
 
+def createSubparser(subparser, func, argList):
+    parser = subparser.add_parser(func.__name__)
+    for arg in argList:
+        parser.add_argument(f"--{arg}", required=True)
+    parser.set_defaults(func=func)
+
+def getImap(args):
+    imap = imaplib.IMAP4_SSL(args.imapServer)
+    imap.login(args.imapUser, args.imapPassword)
+    imap.select(args.imapInbox)
+    return imap
+
+def getEmail(messageNumber, args):
+    imap = getImap(args)
+    _, data = imap.fetch(messageNumber,'(RFC822)')
+    mail = email.message_from_bytes(data[0][1])
+    imap.close()
+    return mail
+
+def writeMessageToFile(path, mail):
+    fileName = getFileName(mail['subject'])
+    with open(f"{path}/{fileName}","w") as f:
+        f.write(mail.as_string())
+
+def getFileName(subject):
+    patchNumber = str(getPatchNumber(subject))
+    commitMessage = getCommitMessage(subject)
+    return f"{patchNumber.zfill(5)}-{commitMessage}.patch"
+
+def getPatchNumber(subject):
+    PatchSection = regexListMatch(SUBJECT_REGEXES, subject).group()
+    patchRe = re.search("\[PATCH (.+?)/[0-9]+\]",PatchSection)
+    currentPatchCount = int(patchRe.group(1)) if patchRe else 1
+    return currentPatchCount
+
+def ensureFolder(folderPath):
+    if path.exists(folderPath) and path.isdir(folderPath) and len(os.listdir(folderPath)) == 0:
+        pass
+    elif path.exists(folderPath) and (path.isdir(folderPath) and len(os.listdir(folderPath)) > 0) or (not path.isdir(folderPath)):
+        shutil.rmtree(folderPath)
+        os.mkdir(folderPath)
+    else:
+        os.mkdir(folderPath)
+
 # initialize argument parsers
 parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers()
 
 # to send notifications, used by jenkins
-def sendNoticiationMails(args):
-    mailserver = smtplib.SMTP(args.smtp_server,args.smtp_port)
+def notificationMailer(args):
+    mailserver = smtplib.SMTP(args.smtpServer,args.smtpPort)
     mailserver.ehlo()
     mailserver.starttls()
-    mailserver.login(args.smtp_user, args.smtp_password)
+    mailserver.login(args.smtpUser, args.smtpPassword)
 
     jenv = Environment(loader = FileSystemLoader(f"templates/{args.template}"))
     env = os.environ
 
     msg = EmailMessage()
     msg['Subject'] = jenv.get_template("subject.jinja").render(**env)
-    msg['From'] = args.smtp_user
+    msg['From'] = args.smtpUser
     msg['To'] = args.to
     msg.set_content(jenv.get_template("body.jinja").render(**env))
 
@@ -41,14 +89,7 @@ def sendNoticiationMails(args):
 
 # add sendNotificationMails to subparser
 # example call: python mailer.py notificationMailer <Arguments as shown below>
-spSendNoticiationMails = subparser.add_parser('notificationMailer')
-spSendNoticiationMails.add_argument("--smtp_server", required=True)
-spSendNoticiationMails.add_argument("--smtp_port", required=True)
-spSendNoticiationMails.add_argument("--smtp_user", required=True)
-spSendNoticiationMails.add_argument("--smtp_password", required=True)
-spSendNoticiationMails.add_argument("--to", required=True)
-spSendNoticiationMails.add_argument("--template", required=True)
-spSendNoticiationMails.set_defaults(func=sendNoticiationMails)
+createSubparser(subparser, notificationMailer, ["smtpServer", "smtpPort", "smtpUser", "smtpPassword","to","template"])
 
 # patch formatter before sending email
 def insertBranchInSubject(subject, branch):
@@ -72,10 +113,7 @@ def patchFormatter(args):
         with open(f"{path}/{file}","w") as f:
             f.writelines(data)
 
-spPatchFormatter = subparser.add_parser('patchFormatter')
-spPatchFormatter.add_argument("--path", help = "path to patches", required=True)
-spPatchFormatter.add_argument("--branch", help = "branch to insert", required=True)
-spPatchFormatter.set_defaults(func=patchFormatter)
+createSubparser(subparser, patchFormatter, ["path","branch"])
 
 # patch branch identifier
 def getBranchFromSubject(subject):
@@ -91,17 +129,19 @@ def patchBranchIdentifier(args):
                 break
     print(branch)
 
-spPatchBranchIdentifier = subparser.add_parser('patchBranchIdentifier')
-spPatchBranchIdentifier.add_argument("--path", help = "path to patches", required=True)
-spPatchBranchIdentifier.set_defaults(func=patchBranchIdentifier)
+createSubparser(subparser, patchBranchIdentifier, ["path"])
 
 # patch reformatter after recieving the mail
+def getCommitMessage(subject):
+    return subject.strip().split('|||')[-1].strip().split('CommitMessage:')[-1].strip()
+
 def removeBranchNameFromSubject(subject):
     subject = subject.split('Subject:',1)[-1].strip()
-    commitMessage = subject.strip().split('|||')[-1].strip().split('CommitMessage:')[-1].strip()
+    commitMessage = getCommitMessage(subject)
     patchRegex = regexListMatch(SUBJECT_REGEXES, subject)
     patchString = patchRegex.group()
     return f"Subject: {patchString} {commitMessage}\n"
+
 
 def patchReFormatter(args):
     path = args.path
@@ -118,41 +158,31 @@ def patchReFormatter(args):
         with open(f"{path}/{file}","w") as f:
             f.writelines(data)
 
-
-spPatchReFormatter = subparser.add_parser('patchReFormatter')
-spPatchReFormatter.add_argument("--path", help = "path to patches", required=True)
-spPatchReFormatter.set_defaults(func=patchReFormatter)
-
+createSubparser(subparser, patchReFormatter, ["path"])
 
 # download Mail from inbox
 def downloadPatchMail(args):
     path = args.path
-    messsageNumber = args.messageNumber
-    imapServer = args.imap_server
-    imapPort = args.imap_port
-    imapUser = args.imap_user
-    imapPassword = args.imap_password
-    imapInbox =  args.imap_inbox
-    imap = imaplib.IMAP4_SSL(imapServer)
-    imap.login(imapUser, imapPassword)
-    imap.select(imapInbox)
-    tmp, data = imap.fetch(messsageNumber, '(RFC822)')
-    f = open(path, "w")
-    f.write(data[0][1].decode('utf-8'))
-    f.close()
-    imap.close()
+    ensureFolder(path)
+    messageNumber = args.messageNumber
+    imap = getImap(args)
+    mail = getEmail(messageNumber, args)
+    PatchSection = regexListMatch(SUBJECT_REGEXES, mail['subject']).group()
+    patchRe = re.search("\[PATCH [0-9]+/(.+?)\]",PatchSection)
+    totalPatchCount = int(patchRe.group(1)) if patchRe else 1
+    print(totalPatchCount)
+    messageId = mail['Message-Id']
+    if totalPatchCount == 1:
+        writeMessageToFile(path, mail)
+    else:
+        writeMessageToFile(path, mail)
+        messageNumbers = imap.search(None,'HEADER', 'In-Reply-To', messageId)
+        messageNumbers = messageNumbers[1][0].decode('utf-8').split()
+        for messageNumber in messageNumbers:
+            mail = getEmail(messageNumber, args)
+            writeMessageToFile(path,mail)
 
-
-spDownloadPatchMail = subparser.add_parser('downloadPatchMail')
-spDownloadPatchMail.add_argument("--path", help = "path to patches", required=True)
-spDownloadPatchMail.add_argument("--messageNumber", help = "Message Number from Jenkins", required=True)
-spDownloadPatchMail.add_argument("--imap_server", required=True)
-spDownloadPatchMail.add_argument("--imap_port", required=True)
-spDownloadPatchMail.add_argument("--imap_user", required=True)
-spDownloadPatchMail.add_argument("--imap_password", required=True)
-spDownloadPatchMail.add_argument("--imap_inbox", required=True)
-
-spDownloadPatchMail.set_defaults(func=downloadPatchMail)
+createSubparser(subparser, downloadPatchMail, ["path","messageNumber","imapServer","imapPort","imapUser","imapPassword","imapInbox"])
 
 # download and reformat patch
 
@@ -161,36 +191,26 @@ def downloadReFormat(args):
     patchReFormatter(args)
     pass
 
-spDownloadReFormat = subparser.add_parser('downloadReFormat')
-spDownloadReFormat.add_argument("--path", help = "path to patches", required=True)
-spDownloadReFormat.add_argument("--messageNumber", help = "Message Number from Jenkins", required=True)
-spDownloadReFormat.add_argument("--imap_server", required=True)
-spDownloadReFormat.add_argument("--imap_port", required=True)
-spDownloadReFormat.add_argument("--imap_user", required=True)
-spDownloadReFormat.add_argument("--imap_password", required=True)
-spDownloadReFormat.add_argument("--imap_inbox", required=True)
+createSubparser(subparser, downloadReFormat, ["path","messageNumber","imapServer","imapPort","imapUser","imapPassword","imapInbox"])
 
-spDownloadReFormat.set_defaults(func=downloadReFormat)
+# get the branch name for a given message number
+def checkMailForBranch(args):
+    mail = getEmail(args.messageNumber, args)
+    branchName = getBranchFromSubject(mail['subject'])
+    print(branchName)
 
+createSubparser(subparser, checkMailForBranch, ["messageNumber","imapServer","imapPort","imapUser","imapPassword","imapInbox"])
 
-# download and reformat patch
+# Check if Mail should trigger a Job
 
-def downloadAndGetBranch(args):
-    downloadPatchMail(args)
-    patchBranchIdentifier(args)
-    pass
+def checkMailForJobTrigger(args):
+    mail = getEmail(args.messageNumber, args)
+    if 'In-Reply-To' in mail:
+        sys.exit(1)
+    else:
+        sys.exit()
 
-spDownloadAndGetBranch = subparser.add_parser('downloadAndGetBranch')
-spDownloadAndGetBranch.add_argument("--path", help = "path to patches", required=True)
-spDownloadAndGetBranch.add_argument("--messageNumber", help = "Message Number from Jenkins", required=True)
-spDownloadAndGetBranch.add_argument("--imap_server", required=True)
-spDownloadAndGetBranch.add_argument("--imap_port", required=True)
-spDownloadAndGetBranch.add_argument("--imap_user", required=True)
-spDownloadAndGetBranch.add_argument("--imap_password", required=True)
-spDownloadAndGetBranch.add_argument("--imap_inbox", required=True)
-
-spDownloadAndGetBranch.set_defaults(func=downloadAndGetBranch)
-
+createSubparser(subparser, checkMailForJobTrigger, ["messageNumber","imapServer","imapPort","imapUser","imapPassword","imapInbox"])
 
 if __name__ == '__main__':
     args = parser.parse_args()

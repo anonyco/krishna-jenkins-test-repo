69cbf2e79e008165ea25cff448075a0545decc7b
krishna <krishna@git.rightend.com>
2022-05-02 13:26:22 -0400

    additional logging

--- git-send-email.sh
+++ git-send-email.sh
@@ -3,4 +3,5 @@ currentBranch=$(git branch --show-current)
 echo "Current Branch: $currentBranch"
 commitCount=$(git rev-list --count $currentBranch ^origin/$currentBranch)
 echo "Branch is behing origin by $commitCount commits"
+echo "Pushing $commitCount commits to Build server"
 git format-patch -o outgoing/ -$commitCount

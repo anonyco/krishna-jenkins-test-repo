f451dcbfe75f8d4c92f7e113123908db47fedcec
Jack <wowzeryest@gmail.com>
2022-05-12 11:19:12 -0400

    IFS Improvement to prevent splitting upon spaces
    E.x. spaces in passwords would have completely bonked this script prior to this commit because the password configuration line would be split at the space into two separately exported variables, which would cause all sorts of issues. This has been partially fixed so that, now, only new lines in quoted entries in the configuration file bonk this script.

--- mailer/mailer.sh
+++ mailer/mailer.sh
@@ -14,7 +14,7 @@ FUNCTION_TYPE=$1
 shift
 
 sed 's/\r$//' ${SMTP_FILE} > smtp_creds.txt
-export $(cat smtp_creds.txt | xargs)
+IFS= eval 'export $(cat smtp_creds.txt | xargs)'
 
 notificationMailer () {
     if [[ $# -lt 2 ]]
